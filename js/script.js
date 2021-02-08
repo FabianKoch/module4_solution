12cf13e5d56def92b08a56ad12d83a79d852638d
Fabian Koch <feb_89_@hotmail.com>
2021-02-08 23:18:29 +0100

    Lecture 41: introduction to functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-var x = "Hello World!";
+var message = "in global";
+console.log("global: message = " + message);/*message = "in global" as defined in the global area*/
+
+var a = function () {
+    var message = "inside a";
+    console.log("a: message = " + message);/*message is = "inside a" as it's define in the function a*/
+
+    function b () {
+        console.log("b: message = " + message);/*function b is defined in function a and is equal to "inside a"*/
+    }
+
+    b();/*this will log "b: message = inside a"*/
+}
+
+a();/*this will log "a: message = inside a" and "b: message = inside a"*/
