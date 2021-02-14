8110274c66ad5ba9390e198dd0049ce2fad819af
Fabian Koch <feb_89_@hotmail.com>
2021-02-14 09:54:04 +0100

    Add javascript types

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var message = "in global";
-console.log("global: message = " + message);/*message = "in global" as defined in the global area*/
+console.log("global: message = " + message);
 
 var a = function () {
     var message = "inside a";
-    console.log("a: message = " + message);/*message is = "inside a" as it's define in the function a*/
+    console.log("a: message = " + message);
 
     function b () {
-        console.log("b: message = " + message);/*function b is defined in function a and is equal to "inside a"*/
+        console.log("b: message = " + message);
     }
 
-    b();/*this will log "b: message = inside a"*/
+    b();
 }
 
-a();/*this will log "a: message = inside a" and "b: message = inside a"*/
+a();
