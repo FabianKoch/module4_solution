29cf3a7556c6027a3d7d5bba15ee3c4ca24a7558
Fabian Koch <feb_89_@hotmail.com>
2021-02-14 18:10:35 +0100

    Add comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Default values
 function orderChickenWith(sideDish) {
-    sideDish = sideDish || "whatever!";
+    sideDish = sideDish || "whatever!";/*if sideDish is cohorced to false it takes the second statement because it's true*/
     console.log("Chicken with " + sideDish);
 }
 
