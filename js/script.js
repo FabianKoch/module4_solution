80dddf4983529f0006e8fe940afc98b494644004
Fabian Koch <feb_89_@hotmail.com>
2021-02-19 17:33:55 +0100

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-// Function constructors
+// Function constructors are usually named with a capital letter, in this case "Circle"
 function Circle (radius) {
     this.radius = radius;
 }
 
+/*this part should not be in the function so that we don't create the area with each new object that we create*/
 Circle.prototype.getArea =
     function () {
-        return Math.PI * Math.pow(this.radius, 2);
+        return Math.PI * Math.pow(this.radius, 2);/*raised by the power of two*/
     };
 
-
-var myCircle = new Circle(10);
+/*we use a function to create a new object
+Function constructors should start with a capital letter, like "Circle" to be recognized more easily*/
+var myCircle = new Circle(10);/*"new" makes "this" point to the circle function*/
 console.log(myCircle.getArea());
 
 var myOtherCircle = new Circle(20);
