13a07598af15c4fa3a7452a3c1699f041a27e60d
Fabian Koch <feb_89_@hotmail.com>
2021-02-28 18:53:51 +0100

    Comments for Lecture 49

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Object literals and "this"
-var literalCircle = {
+var literalCircle = { /*opening the curly braces generates a new object*/
     radius: 10,
 
     getArea: function () {
-        var self = this;
+        var self = this;/*we define self in the first function so that we can use it in the second*/
         console.log(this);
 
-        var increaseRadius = function () {
-            self.radius = 20;
+        var increaseRadius = function () {/*define function to increase radius*/
+            self.radius = 20;/*we must use "self" because "this" would point to the global object because we have a function within a function*/
         };
-        increaseRadius();
+        increaseRadius();/*invoke function*/
         console.log(this.radius);
 
         return Math.PI * Math.pow(this.radius, 2);
